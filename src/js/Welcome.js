41aef2ed46f3a193ebac38efdaf5922ddafb207b
Manideep Shanigaram <manideepshanigaram.sm@gmail.com>
2023-01-27 21:19:03 -0600

    first push

---
+++ src/js/Welcome.js
@@ -0,0 +1,50 @@
+
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+function Welcome(){
+
+    const [welcomMessage,setMessage] = useState("")
+    const [nums,setNums] = useState([]); 
+
+
+  
+  const peopleapi = ()=>{
+    Axios.get('https://azurewelcome-mani.azurewebsites.net/v1/people')
+    .then((res)=> {
+      console.log(res);
+      setNums(res.data);
+    }) 
+  };
+
+    const iterableList = nums.map(
+        (i)=>{
+          // return `<li style="color: blue; font-weight: bold">${i.typeId}</li>`
+          return<li> {i}</li>;
+        }
+      );
+
+
+    const firstapi = ()=>{
+        Axios.get('https://azurewelcome-mani.azurewebsites.net/v1/message')
+        .then((res)=> {
+          console.log(res);
+          setMessage(res.data);
+
+        }) 
+      };
+
+
+    return (
+        <div>
+            <button onClick={firstapi}>Click here to see welcome message</button>
+            <h1 >{welcomMessage}</h1>
+            <hr></hr>
+            <button onClick={peopleapi}>Click here to see People list</button>
+            <h1 >{iterableList}</h1>
+        </div>
+    )
+}
+
+
+export default Welcome;
